tursodatabase/limbo

Treat table-valued functions as tables
With this change, the following two queries are considered equivalent:
```sql
SELECT value FROM generate_series(5, 50);
SELECT value FROM generate_series WHERE start = 5 AND stop = 50;
```
Arguments passed in parentheses to the virtual table name are now
matched to hidden columns.

Column references are still not supported as table-valued function
arguments. The only difference is that previously, a query like:
```sql
SELECT one.value, series.value
FROM (SELECT 1 AS value) one, generate_series(one.value, 3) series;
```
would cause a panic. Now, it returns a proper error message instead.

Adding support for column references is more nuanced for two main
reasons:
- We need to ensure that in joins where a TVF depends on other tables,
those other tables are processed first. For example, in:
```sql
SELECT one.value, series.value
FROM generate_series(one.value, 3) series, (SELECT 1 AS value) one;
```
the one table must be processed by the top-level loop, and series must
be nested.
- For outer joins involving TVFs, the arguments must be treated as ON
predicates, not WHERE predicates.

core/ext/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "fs")]
 mod dynamic;
 mod vtab_xconnect;
+use crate::vtab::VirtualTable;
 #[cfg(all(target_os = "linux", feature = "io_uring"))]
 use crate::UringIO;
 use crate::{function::ExternalFunc, Connection, Database, LimboError, IO};
@@ -151,6 +152,13 @@ impl Connection {
             .borrow_mut()
             .vtab_modules
             .insert(name.to_string(), vmodule.into());
+        if kind == VTabKind::TableValuedFunction {
+            if let Ok(vtab) = VirtualTable::function(name, &self.syms.borrow()) {
+                self.schema.write().add_virtual_table(vtab);
+            } else {
+                return ResultCode::Error;
+            }
+        }
         ResultCode::OK
     }
 

core/translate/planner.rs
@@ -18,6 +18,7 @@ use crate::{
     vdbe::{builder::TableRefIdCounter, BranchOffset},
     Result,
 };
+use limbo_sqlite3_parser::ast::Literal::Null;
 use limbo_sqlite3_parser::ast::{
     self, As, Expr, FromClause, JoinType, Limit, Materialized, QualifiedName, TableInternalId,
     UnaryOperator, With,
@@ -236,6 +237,7 @@ fn parse_from_clause_table<'a>(
     schema: &Schema,
     table: ast::SelectTable,
     table_references: &mut TableReferences,
+    out_where_clause: &mut Vec<WhereTerm>,
     ctes: &mut Vec<JoinedTable>,
     syms: &SymbolTable,
     table_ref_counter: &mut TableRefIdCounter,
@@ -246,8 +248,10 @@ fn parse_from_clause_table<'a>(
             table_references,
             ctes,
             table_ref_counter,
+            out_where_clause,
             qualified_name,
             maybe_alias,
+            None,
         ),
         ast::SelectTable::Select(subselect, maybe_alias) => {
             let Plan::Select(subplan) = prepare_select_plan(
@@ -279,31 +283,16 @@ fn parse_from_clause_table<'a>(
             ));
             Ok(())
         }
-        ast::SelectTable::TableCall(qualified_name, maybe_args, maybe_alias) => {
-            let normalized_name = &normalize_ident(qualified_name.name.0.as_str());
-            let vtab = crate::VirtualTable::function(normalized_name, maybe_args, syms)?;
-            let alias = maybe_alias
-                .as_ref()
-                .map(|a| match a {
-                    ast::As::As(id) => id.0.clone(),
-                    ast::As::Elided(id) => id.0.clone(),
-                })
-                .unwrap_or(normalized_name.to_string());
-
-            table_references.add_joined_table(JoinedTable {
-                op: Operation::Scan {
-                    iter_dir: IterationDirection::Forwards,
-                    index: None,
-                },
-                join_info: None,
-                table: Table::Virtual(vtab),
-                identifier: alias,
-                internal_id: table_ref_counter.next(),
-                col_used_mask: ColumnUsedMask::new(),
-            });
-
-            Ok(())
-        }
+        ast::SelectTable::TableCall(qualified_name, maybe_args, maybe_alias) => parse_table(
+            schema,
+            table_references,
+            ctes,
+            table_ref_counter,
+            out_where_clause,
+            qualified_name,
+            maybe_alias,
+            maybe_args,
+        ),
         _ => todo!(),
     }
 }
@@ -313,8 +302,10 @@ fn parse_table(
     table_references: &mut TableReferences,
     ctes: &mut Vec<JoinedTable>,
     table_ref_counter: &mut TableRefIdCounter,
+    out_where_clause: &mut Vec<WhereTerm>,
     qualified_name: QualifiedName,
     maybe_alias: Option<As>,
+    maybe_args: Option<Vec<Expr>>,
 ) -> Result<()> {
     let normalized_qualified_name = normalize_ident(qualified_name.name.0.as_str());
     // Check if the FROM clause table is referring to a CTE in the current scope.
@@ -336,7 +327,16 @@ fn parse_table(
                 ast::As::Elided(id) => id,
             })
             .map(|a| a.0);
+        let internal_id = table_ref_counter.next();
         let tbl_ref = if let Table::Virtual(tbl) = table.as_ref() {
+            if let Some(args) = maybe_args {
+                transform_args_into_where_terms(
+                    args,
+                    internal_id,
+                    out_where_clause,
+                    table.as_ref(),
+                )?;
+            }
             Table::Virtual(tbl.clone())
         } else if let Table::BTree(table) = table.as_ref() {
             Table::BTree(table.clone())
@@ -352,7 +352,7 @@ fn parse_table(
             },
             table: tbl_ref,
             identifier: alias.unwrap_or(normalized_qualified_name),
-            internal_id: table_ref_counter.next(),
+            internal_id,
             join_info: None,
             col_used_mask: ColumnUsedMask::new(),
         });
@@ -386,6 +386,72 @@ fn parse_table(
     }
 
     crate::bail_parse_error!("Table {} not found", normalized_qualified_name);
+}
+
+fn transform_args_into_where_terms(
+    args: Vec<Expr>,
+    internal_id: TableInternalId,
+    out_where_clause: &mut Vec<WhereTerm>,
+    table: &Table,
+) -> Result<()> {
+    let mut args_iter = args.into_iter();
+    let mut hidden_count = 0;
+    for (i, col) in table.columns().iter().enumerate() {
+        if !col.hidden {
+            continue;
+        }
+        hidden_count += 1;
+
+        if let Some(arg_expr) = args_iter.next() {
+            if contains_column_reference(&arg_expr)? {
+                crate::bail_parse_error!(
+                    "Column references are not supported as table-valued function arguments yet"
+                );
+            }
+            let column_expr = Expr::Column {
+                database: None,
+                table: internal_id,
+                column: i,
+                is_rowid_alias: col.is_rowid_alias,
+            };
+            let expr = match arg_expr {
+                Expr::Literal(Null) => Expr::IsNull(Box::new(column_expr)),
+                other => Expr::Binary(
+                    Box::new(column_expr),
+                    ast::Operator::Equals,
+                    Box::new(other),
+                ),
+            };
+            out_where_clause.push(WhereTerm {
+                expr,
+                from_outer_join: None,
+                consumed: Cell::new(false),
+            });
+        }
+    }
+
+    if args_iter.next().is_some() {
+        return Err(crate::LimboError::ParseError(format!(
+            "Too many arguments for {}: expected at most {}, got {}",
+            table.get_name(),
+            hidden_count,
+            hidden_count + 1 + args_iter.count()
+        )));
+    }
+
+    Ok(())
+}
+
+fn contains_column_reference(top_level_expr: &Expr) -> Result<bool> {
+    let mut contains = false;
+    walk_expr(top_level_expr, &mut |expr: &Expr| -> Result<()> {
+        match expr {
+            Expr::Id(_) | Expr::Qualified(_, _) | Expr::Column { .. } => contains = true,
+            _ => {}
+        };
+        Ok(())
+    })?;
+    Ok(contains)
 }
 
 pub fn parse_from<'a>(
@@ -478,6 +544,7 @@ pub fn parse_from<'a>(
         schema,
         select_owned,
         table_references,
+        out_where_clause,
         &mut ctes_as_subqueries,
         syms,
         table_ref_counter,
@@ -716,6 +783,7 @@ fn parse_join<'a>(
         schema,
         table,
         table_references,
+        out_where_clause,
         ctes,
         syms,
         table_ref_counter,

orrinfrazier/turso

core/ext/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "fs")]
 mod dynamic;
 mod vtab_xconnect;
+use crate::vtab::VirtualTable;
 #[cfg(all(target_os = "linux", feature = "io_uring"))]
 use crate::UringIO;
 use crate::{function::ExternalFunc, Connection, Database, LimboError, IO};
@@ -148,6 +149,13 @@ impl Connection {
             .borrow_mut()
             .vtab_modules
             .insert(name.to_string(), vmodule.into());
+        if kind == VTabKind::TableValuedFunction {
+            if let Ok(vtab) = VirtualTable::function(name, &self.syms.borrow()) {
+                self.schema.borrow_mut().add_virtual_table(vtab);
+            } else {
+                return ResultCode::Error;
+            }
+        }
         ResultCode::OK
     }
 

core/translate/planner.rs
@@ -19,6 +19,7 @@ use crate::{
     vdbe::{builder::TableRefIdCounter, BranchOffset},
     Result,
 };
+use turso_sqlite3_parser::ast::Literal::Null;
 use turso_sqlite3_parser::ast::{
     self, As, Expr, FromClause, JoinType, Limit, Materialized, QualifiedName, TableInternalId,
     UnaryOperator, With,
@@ -243,6 +244,7 @@ fn parse_from_clause_table(
     schema: &Schema,
     table: ast::SelectTable,
     table_references: &mut TableReferences,
+    out_where_clause: &mut Vec<WhereTerm>,
     ctes: &mut Vec<JoinedTable>,
     syms: &SymbolTable,
     table_ref_counter: &mut TableRefIdCounter,
@@ -253,8 +255,10 @@ fn parse_from_clause_table(
             table_references,
             ctes,
             table_ref_counter,
+            out_where_clause,
             qualified_name,
             maybe_alias,
+            None,
         ),
         ast::SelectTable::Select(subselect, maybe_alias) => {
             let Plan::Select(subplan) = prepare_select_plan(
@@ -286,42 +290,30 @@ fn parse_from_clause_table(
             ));
             Ok(())
         }
-        ast::SelectTable::TableCall(qualified_name, maybe_args, maybe_alias) => {
-            let normalized_name = &normalize_ident(qualified_name.name.0.as_str());
-            let vtab = crate::VirtualTable::function(normalized_name, maybe_args, syms)?;
-            let alias = maybe_alias
-                .as_ref()
-                .map(|a| match a {
-                    ast::As::As(id) => id.0.clone(),
-                    ast::As::Elided(id) => id.0.clone(),
-                })
-                .unwrap_or(normalized_name.to_string());
-
-            table_references.add_joined_table(JoinedTable {
-                op: Operation::Scan {
-                    iter_dir: IterationDirection::Forwards,
-                    index: None,
-                },
-                join_info: None,
-                table: Table::Virtual(vtab),
-                identifier: alias,
-                internal_id: table_ref_counter.next(),
-                col_used_mask: ColumnUsedMask::default(),
-            });
-
-            Ok(())
-        }
+        ast::SelectTable::TableCall(qualified_name, maybe_args, maybe_alias) => parse_table(
+            schema,
+            table_references,
+            ctes,
+            table_ref_counter,
+            out_where_clause,
+            qualified_name,
+            maybe_alias,
+            maybe_args,
+        ),
         _ => todo!(),
     }
 }
 
+#[allow(clippy::too_many_arguments)]
 fn parse_table(
     schema: &Schema,
     table_references: &mut TableReferences,
     ctes: &mut Vec<JoinedTable>,
     table_ref_counter: &mut TableRefIdCounter,
+    out_where_clause: &mut Vec<WhereTerm>,
     qualified_name: QualifiedName,
     maybe_alias: Option<As>,
+    maybe_args: Option<Vec<Expr>>,
 ) -> Result<()> {
     let normalized_qualified_name = normalize_ident(qualified_name.name.0.as_str());
     // Check if the FROM clause table is referring to a CTE in the current scope.
@@ -343,7 +335,16 @@ fn parse_table(
                 ast::As::Elided(id) => id,
             })
             .map(|a| a.0);
+        let internal_id = table_ref_counter.next();
         let tbl_ref = if let Table::Virtual(tbl) = table.as_ref() {
+            if let Some(args) = maybe_args {
+                transform_args_into_where_terms(
+                    args,
+                    internal_id,
+                    out_where_clause,
+                    table.as_ref(),
+                )?;
+            }
             Table::Virtual(tbl.clone())
         } else if let Table::BTree(table) = table.as_ref() {
             Table::BTree(table.clone())
@@ -359,7 +360,7 @@ fn parse_table(
             },
             table: tbl_ref,
             identifier: alias.unwrap_or(normalized_qualified_name),
-            internal_id: table_ref_counter.next(),
+            internal_id,
             join_info: None,
             col_used_mask: ColumnUsedMask::default(),
         });
@@ -393,6 +394,72 @@ fn parse_table(
     }
 
     crate::bail_parse_error!("no such table: {}", normalized_qualified_name);
+}
+
+fn transform_args_into_where_terms(
+    args: Vec<Expr>,
+    internal_id: TableInternalId,
+    out_where_clause: &mut Vec<WhereTerm>,
+    table: &Table,
+) -> Result<()> {
+    let mut args_iter = args.into_iter();
+    let mut hidden_count = 0;
+    for (i, col) in table.columns().iter().enumerate() {
+        if !col.hidden {
+            continue;
+        }
+        hidden_count += 1;
+
+        if let Some(arg_expr) = args_iter.next() {
+            if contains_column_reference(&arg_expr)? {
+                crate::bail_parse_error!(
+                    "Column references are not supported as table-valued function arguments yet"
+                );
+            }
+            let column_expr = Expr::Column {
+                database: None,
+                table: internal_id,
+                column: i,
+                is_rowid_alias: col.is_rowid_alias,
+            };
+            let expr = match arg_expr {
+                Expr::Literal(Null) => Expr::IsNull(Box::new(column_expr)),
+                other => Expr::Binary(
+                    Box::new(column_expr),
+                    ast::Operator::Equals,
+                    Box::new(other),
+                ),
+            };
+            out_where_clause.push(WhereTerm {
+                expr,
+                from_outer_join: None,
+                consumed: Cell::new(false),
+            });
+        }
+    }
+
+    if args_iter.next().is_some() {
+        return Err(crate::LimboError::ParseError(format!(
+            "Too many arguments for {}: expected at most {}, got {}",
+            table.get_name(),
+            hidden_count,
+            hidden_count + 1 + args_iter.count()
+        )));
+    }
+
+    Ok(())
+}
+
+fn contains_column_reference(top_level_expr: &Expr) -> Result<bool> {
+    let mut contains = false;
+    walk_expr(top_level_expr, &mut |expr: &Expr| -> Result<WalkControl> {
+        match expr {
+            Expr::Id(_) | Expr::Qualified(_, _) | Expr::Column { .. } => contains = true,
+            _ => {}
+        };
+        Ok(WalkControl::Continue)
+    })?;
+    Ok(contains)
 }
 
 pub fn parse_from(
@@ -485,6 +552,7 @@ pub fn parse_from(
         schema,
         select_owned,
         table_references,
+        out_where_clause,
         &mut ctes_as_subqueries,
         syms,
         table_ref_counter,
@@ -723,6 +791,7 @@ fn parse_join(
         schema,
         table,
         table_references,
+        out_where_clause,
         ctes,
         syms,
         table_ref_counter,